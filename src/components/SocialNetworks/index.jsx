ed33ee1a9fdca8a5bf0c0ade792a8578dba75334
Akzam Khamitov <makoron3002@gmail.com>
2023-04-09 23:42:47 +0600

    added SocialNetworks

---
+++ src/components/SocialNetworks/index.jsx
@@ -0,0 +1,18 @@
+import styles from './SocialNetworks.module.scss';
+
+function SocialNetworks({ list }) {
+	return (
+		<div className={ styles.socialNetworkIcons }>
+			{
+				list.map(
+					item => 
+						<a key={item.id} href={item.url}>
+							<i className={item.icon}></i>
+						</a>
+				)
+			}
+		</div>
+	);
+}
+
+export default SocialNetworks;
