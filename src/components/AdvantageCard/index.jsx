9233cb2047f345a3150e2ba77f343b89db1e3fa7
Akzam Khamitov <makoron3002@gmail.com>
2023-04-16 16:41:44 +0600

    renamed component Advantage to AdvantageCard

---
+++ src/components/AdvantageCard/index.jsx
@@ -0,0 +1,15 @@
+import styles from './AdvantageCard.module.scss';
+
+function Advantage({icon, title, text}) {
+    return (
+        <div className={styles.advantage}>
+            <div className={styles.header}>
+                <i className={icon}></i>
+                <h3>{title}</h3>
+            </div>
+            <p>{text}</p>
+        </div>
+    );
+}
+
+export default Advantage;
